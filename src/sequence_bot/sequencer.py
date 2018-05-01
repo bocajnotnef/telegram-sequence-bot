29a3ecb90e3a93ea10d30e30a73055ce636ce3f9
Jake Fenton <jake@fenton.io>
2018-05-01 16:49:09 -0400

    Sequencer informs of new word

--- src/sequence_bot/sequencer.py
+++ src/sequence_bot/sequencer.py
@@ -111,7 +111,7 @@ def validate(bot, update):
         else:
             if sequence.validate(update.message.text):
                 if update.message.text in DICTIONARY:
-                    bot.send_message(RESPONDING_CHAT, f"Hey @{user}, that's a scrabble word!")
+                    bot.send_message(RESPONDING_CHAT, f"Hey @{user}, {update.message.text} is a scrabble word!")
             else:
                 bot.send_message(RESPONDING_CHAT, f"@{user} WRONG")
 
